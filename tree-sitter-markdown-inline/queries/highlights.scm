3a954ecdf87f2cb24022b601f9ec57751e897982
Ryan McKinney <rydamckinney@gmail.com>
2023-09-21 20:15:07 -0400

    fix improper node error on swift

--- tree-sitter-markdown-inline/queries/highlights.scm
+++ tree-sitter-markdown-inline/queries/highlights.scm
@@ -1,4 +1,4 @@
-;; From nvim-treesitter/nvim-treesitter
+;From nvim-treesitter/nvim-treesitter
 [
   (code_span)
   (link_title)
@@ -29,10 +29,7 @@
   (hard_line_break)
 ] @string.escape
 
-; ")" not part of query because of
-; https://github.com/nvim-treesitter/nvim-treesitter/issues/2206
-; TODO: Find better fix for this
 (image ["!" "[" "]" "("] @punctuation.delimiter)
 (inline_link ["[" "]" "("] @punctuation.delimiter)
 (shortcut_link ["[" "]"] @punctuation.delimiter)
-(wiki_link ["[" "|" "]"] @punctuation.delimiter)
+;(wiki_link ["[" "|" "]"] @punctuation.delimiter)
